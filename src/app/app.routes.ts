09290d7fc9dada52881af4d3649cc94d57938830
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2025-01-19 20:43:42 -0300

    style: format files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './screens/home/home.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
         path: '',
-        component: AppComponent,
-        children: [
-            {
-                path: '',
-                component: HomeComponent
-            }
-        ]
-    },
-
+        component: HomeComponent,
+      },
+    ],
+  },
 ];
